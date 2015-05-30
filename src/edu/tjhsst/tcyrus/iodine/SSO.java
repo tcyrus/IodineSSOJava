59b92d24c81abf25bc40bf8e9f91ef3039654436
TCYRUS <tcyrus@users.noreply.github.com>
2015-05-29 21:17:24 -0400

    Javadoc + Rename Package

--- src/edu/tjhsst/tcyrus/iodine/SSO.java
+++ src/edu/tjhsst/tcyrus/iodine/SSO.java
@@ -1,4 +1,4 @@
-package edu.tjhsst.tcyrus.IodineSSOAPI;
+package edu.tjhsst.tcyrus.iodine;
 
 import java.io.BufferedReader;
 import java.io.IOException;
@@ -7,7 +7,6 @@ import java.io.InputStreamReader;
 import java.io.UnsupportedEncodingException;
 import java.net.*;
 import java.util.List;
-import java.util.Scanner;
 
 import javax.net.ssl.HttpsURLConnection;
 
@@ -16,28 +15,46 @@ import org.apache.commons.codec.binary.Base64;
 import org.json.JSONException;
 import org.json.JSONObject;
 
+/** Iodine SSO API
+ *
+ * @author Timothy Cyrus
+ */
 public class SSO {
-	public static void main(String[] args) throws IOException {
-		System.out.println(SSOUrl("App Name",new URL("http://localhost/")).toString());
-		Scanner scan=new Scanner(System.in);
-		System.out.println("Enter SSO Key");
-		String input=scan.next();
-		scan.close();
-		System.out.println(verifySSO(input).toString());
-		System.out.println(login(input));
+	/** Creates A Single Sign On URL
+	 * 
+	 * @param name			Name of Application
+	 * @param callback		Callback URL or Intent URI
+	 * @return URL			URL to launch in Web Browser
+	 * @throws MalformedURLException
+	 * @throws UnsupportedEncodingException
+	 */
+	public static URL SSOUrl(String name, URL callback) throws MalformedURLException, UnsupportedEncodingException {
+		return SSOUrl(name,callback,120);
 	}
-	public static URL SSOUrl(String title, URL callback) throws MalformedURLException, UnsupportedEncodingException {
-		return SSOUrl(title,callback,120);
-	}
-	public static URL SSOUrl(String title, URL callback, int exp) throws MalformedURLException, UnsupportedEncodingException {
+	/** Creates A Single Sign On URL
+	 * 
+	 * @param name			Name of Application
+	 * @param callback		Callback URL or Intent URI
+	 * @param exp			Custom Time for Expiration of Token (SSO Token in POST)
+	 * @return URL			URL to launch in Web Browser
+	 * @throws MalformedURLException
+	 * @throws UnsupportedEncodingException
+	 */
+	public static URL SSOUrl(String name, URL callback, int exp) throws MalformedURLException, UnsupportedEncodingException {
 		int time=(int)(System.currentTimeMillis()/1000L);
 		String data="time="+URLEncoder.encode(Integer.toString(time),"UTF-8")+"&method=GET";
-		data+="&title="+URLEncoder.encode(title,"UTF-8");
+		data+="&title="+URLEncoder.encode(name,"UTF-8");
 	    data+="&return="+URLEncoder.encode(callback.toString(),"UTF-8");
 	    data+="&exp="+URLEncoder.encode(Integer.toString(time+exp),"UTF-8");
 	    String data64=new String(Base64.encodeBase64(data.getBytes()));
 	    return new URL("https://iodine.tjhsst.edu/sso?req="+data64);
 	}
+	/** Verifies SSO Token
+	 * 
+	 * @param sso			SSO Token
+	 * @return JSONObject	JSON returned from Verification
+	 * @throws IOException
+	 */
 	public static JSONObject verifySSO(String sso) throws IOException {
 		URL verifyurl=new URL("https://iodine.tjhsst.edu/ajax/sso/valid_key?sso="+sso);
 		HttpsURLConnection urlConnection=(HttpsURLConnection)verifyurl.openConnection();
@@ -46,6 +63,12 @@ public class SSO {
 		urlConnection.disconnect();
 		return result;
 	}
+	/** Logs In to Iodine using SSO Token
+	 * 
+	 * @param sso			SSO Token
+	 * @return List<String>	List of Cookies
+	 * @throws IOException
+	 */
 	public static List<String> login(String sso) throws IOException {
 		URL loginurl=new URL("https://iodine.tjhsst.edu/login?sso="+sso);
 		HttpsURLConnection urlConnection=(HttpsURLConnection)loginurl.openConnection();
@@ -54,6 +77,13 @@ public class SSO {
 		urlConnection.disconnect();
 		return cookieList;
 	}
+	/** Converts Input Stream to JSONObject
+	 * 
+	 * @param inputStream
+	 * @return JSONObject
+	 * @throws JSONException
+	 * @throws IOException
+	 */
 	private static JSONObject inputStreamToJSON(InputStream inputStream) throws JSONException, IOException {
 		final BufferedReader reader=new BufferedReader(new InputStreamReader(inputStream,"UTF-8"),8);
 		final StringBuilder sb=new StringBuilder();
